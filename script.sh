4c7120a7238b659062b1e18133458e795253d01e
“” <you@example.com>
2025-02-07 10:51:38 +0100

    feat: add user variable

--- script.sh
+++ script.sh
@@ -21,6 +21,7 @@ RESET="\e[0m"
 echo -e "${YELLOW}[*]${RESET} Proyecto Docker Swarm"
 
 # Configuración de nodos
+USER=o22408064
 MANAGER=$1  # Nodo que será el manager
 WORKERS=($2 $3)  # Nodos que serán workers
 
@@ -49,7 +50,7 @@ DOCKER_INSTALL_COMMANDS="
 install_docker() {
     local node="$1"
     echo -e "${YELLOW}[*]${RESET} Instalando Docker en: ${BLUE}$node${RESET}"
-    ssh root@$node "$DOCKER_INSTALL_COMMANDS"
+    ssh $USER@$node "$DOCKER_INSTALL_COMMANDS"
     echo -e "${GREEN}[+]${RESET} Docker instalado en: ${BLUE}$node${RESET}"
 }
 
@@ -58,17 +59,17 @@ initialize_swarm() {
     echo -e "${YELLOW}[*]${RESET} Comprobando si Docker Swarm ya está inicializado en el manager"
 
     # Verificar si el manager ya es parte de un swarm
-    SWARM_STATUS=$(ssh root@$MANAGER "docker info --format '{{.Swarm.LocalNodeState}}'")
-    MANAGER_IP=$(ssh root@$MANAGER "hostname -I | awk '{print \$1}'")
+    SWARM_STATUS=$(ssh $USER@$MANAGER "docker info --format '{{.Swarm.LocalNodeState}}'")
+    MANAGER_IP=$(ssh $USER@$MANAGER "hostname -I | awk '{print \$1}'")
 
     if [ "$SWARM_STATUS" == "active" ]; then
         echo -e "${GREEN}[+]${RESET} Docker Swarm ya está inicializado en el manager"
     else
         echo -e "${YELLOW}[*]${RESET} Inicializando Docker Swarm en el manager"
-        ssh root@$MANAGER "docker swarm init --advertise-addr $MANAGER_IP"
+        ssh $USER@$MANAGER "docker swarm init --advertise-addr $MANAGER_IP"
 
         # Extrae el token de unión para los workers
-        JOIN_TOKEN=$(ssh root@$MANAGER "docker swarm join-token worker -q")
+        JOIN_TOKEN=$(ssh $USER@$MANAGER "docker swarm join-token worker -q")
         echo -e "${GREEN}[+]${RESET} Docker Swarm inicializado en el manager"
     fi
 }
@@ -79,13 +80,13 @@ join_swarm_workers() {
         echo -e "${YELLOW}[*]${RESET} Comprobando si el worker ya está unido al Swarm: ${BLUE}$worker${RESET}"
 
         # Verificar si el worker ya está en un swarm
-        SWARM_STATUS=$(ssh root@$worker "docker info --format '{{.Swarm.LocalNodeState}}'")
+        SWARM_STATUS=$(ssh $USER@$worker "docker info --format '{{.Swarm.LocalNodeState}}'")
 
         if [ "$SWARM_STATUS" == "active" ]; then
             echo -e "${GREEN}[+]${RESET} El worker ya está unido al Swarm: ${BLUE}$worker${RESET}"
         else
             echo -e "${YELLOW}[*]${RESET} Uniendo worker al Swarm: ${BLUE}$worker${RESET}"
-            ssh root@$worker "sudo docker swarm join --token $JOIN_TOKEN $MANAGER_IP:2377"
+            ssh $USER@$worker "sudo docker swarm join --token $JOIN_TOKEN $MANAGER_IP:2377"
             echo -e "${GREEN}[+]${RESET} Worker unido al Swarm: ${BLUE}$worker${RESET}"
         fi
     done
@@ -114,16 +115,16 @@ echo -e "${YELLOW}[*]${RESET} Configurar gluster en los servidores"
 install_gluster() {
     local node="$1"
     echo -e "${YELLOW}[*]${RESET} Instalando GlusterFS en: ${BLUE}$node${RESET}"
-    ssh root@$node "sudo apt update && sudo apt install -y glusterfs-server && sudo systemctl start glusterd && sudo systemctl enable glusterd"
+    ssh $USER@$node "sudo apt update && sudo apt install -y glusterfs-server && sudo systemctl start glusterd && sudo systemctl enable glusterd"
     echo -e "${GREEN}[+]${RESET} GlusterFS instalado en: ${BLUE}$node${RESET}"
 }
 
 configure_gluster() {
     echo -e "${YELLOW}[*]${RESET} Configurando GlusterFS"
-    if ssh root@$MANAGER "sudo gluster peer status | grep -q 'Number of Peers: 2'"; then
+    if ssh $USER@$MANAGER "sudo gluster peer status | grep -q 'Number of Peers: 2'"; then
         echo -e "${GREEN}[+]${RESET} GlusterFS ya está configurado"
     else
-        ssh root@$MANAGER "sudo gluster peer probe ${WORKERS[0]} && sudo gluster peer probe ${WORKERS[1]} && sudo gluster pool list"
+        ssh $USER@$MANAGER "sudo gluster peer probe ${WORKERS[0]} && sudo gluster peer probe ${WORKERS[1]} && sudo gluster pool list"
         echo -e "${GREEN}[+]${RESET} GlusterFS configurado"
     fi
 }
@@ -132,15 +133,15 @@ make_gluster_volume() {
     local volume_name="$1"
     local brick_dir="/data/$volume_name"
     echo -e "${YELLOW}[*]${RESET} Creando volumen de GlusterFS: ${BLUE}$volume_name${RESET}"
-    if ssh root@$MANAGER "sudo gluster volume info $volume_name | grep -q 'Status: Started'"; then
+    if ssh $USER@$MANAGER "sudo gluster volume info $volume_name | grep -q 'Status: Started'"; then
         echo -e "${GREEN}[+]${RESET} Volumen de GlusterFS ${BLUE}$volume_name${RESET} ya está creado y en funcionamiento"
     else
-        ssh root@$MANAGER "sudo mkdir -p $brick_dir"
+        ssh $USER@$MANAGER "sudo mkdir -p $brick_dir"
         for worker in "${WORKERS[@]}"; do
-            ssh root@$worker "sudo mkdir -p $brick_dir"
+            ssh $USER@$worker "sudo mkdir -p $brick_dir"
         done
-        ssh root@$MANAGER "sudo gluster volume create $volume_name replica 3 transport tcp ${MANAGER}:${brick_dir} ${WORKERS[0]}:${brick_dir} ${WORKERS[1]}:${brick_dir} force"
-        ssh root@$MANAGER "sudo gluster volume start $volume_name"
+        ssh $USER@$MANAGER "sudo gluster volume create $volume_name replica 3 transport tcp ${MANAGER}:${brick_dir} ${WORKERS[0]}:${brick_dir} ${WORKERS[1]}:${brick_dir} force"
+        ssh $USER@$MANAGER "sudo gluster volume start $volume_name"
         echo -e "${GREEN}[+]${RESET} Volumen de GlusterFS ${BLUE}$volume_name${RESET} creado"
     fi
 }
@@ -149,15 +150,15 @@ mount_gluster() {
     local node="$1"
     local volume_name="$2"
     echo -e "${YELLOW}[*]${RESET} Montando volumen de GlusterFS ${BLUE}$volume_name${RESET} en: ${BLUE}$node${RESET}"
-    if ssh root@$node "mount | grep -q '/mnt/$volume_name'"; then
+    if ssh $USER@$node "mount | grep -q '/mnt/$volume_name'"; then
         echo -e "${GREEN}[+]${RESET} Volumen de GlusterFS ${BLUE}$volume_name${RESET} ya está montado en: ${BLUE}$node${RESET}"
     else
-        ssh root@$node "sudo apt install -y glusterfs-client"
-        ssh root@$node "sudo mkdir -p /mnt/$volume_name"
-        ssh root@$node "sudo mount -t glusterfs ${MANAGER}:/$volume_name /mnt/$volume_name"
+        ssh $USER@$node "sudo apt install -y glusterfs-client"
+        ssh $USER@$node "sudo mkdir -p /mnt/$volume_name"
+        ssh $USER@$node "sudo mount -t glusterfs ${MANAGER}:/$volume_name /mnt/$volume_name"
         echo -e "${GREEN}[+]${RESET} Volumen de GlusterFS ${BLUE}$volume_name${RESET} montado en: ${BLUE}$node${RESET}"
         echo -e "${YELLOW}[*]${RESET} Modificando /etc/fstab para montar el volumen de GlusterFS ${BLUE}$volume_name${RESET} en el arranque"
-        ssh root@$node "echo '${MANAGER}:/$volume_name /mnt/$volume_name glusterfs defaults,_netdev 0 0' | sudo tee -a /etc/fstab"
+        ssh $USER@$node "echo '${MANAGER}:/$volume_name /mnt/$volume_name glusterfs defaults,_netdev 0 0' | sudo tee -a /etc/fstab"
         echo -e "${GREEN}[+]${RESET} Volumen de GlusterFS ${BLUE}$volume_name${RESET} montado en el arranque en: ${BLUE}$node${RESET}"
     fi
 }
@@ -165,10 +166,10 @@ mount_gluster() {
 active_auto_curation() {
     local volume_name="$1"
     echo -e "${YELLOW}[*]${RESET} Activando la curación automática de GlusterFS para el volumen: ${BLUE}$volume_name${RESET}"
-    if ssh root@$MANAGER "sudo gluster volume get $volume_name cluster.self-heal-daemon | grep -q 'on'"; then
+    if ssh $USER@$MANAGER "sudo gluster volume get $volume_name cluster.self-heal-daemon | grep -q 'on'"; then
         echo -e "${GREEN}[+]${RESET} Curación automática ya está activada para el volumen: ${BLUE}$volume_name${RESET}"
     else
-        ssh root@$MANAGER "sudo gluster volume set $volume_name cluster.self-heal-daemon on"
+        ssh $USER@$MANAGER "sudo gluster volume set $volume_name cluster.self-heal-daemon on"
         echo -e "${GREEN}[+]${RESET} Curación automática activada para el volumen: ${BLUE}$volume_name${RESET}"
     fi
 }
@@ -204,14 +205,14 @@ echo -e "${GREEN}[+]${RESET} Configuración de GlusterFS finalizada"
 echo -e "${YELLOW}[*]${RESET} Desplegando servicios con Docker stack"
 
 echo -e "${YELLOW}[*]${RESET} Copiando archivos a la máquina remota"
-scp docker-compose.yml root@$MANAGER:~
-scp -r traefik.yml root@$MANAGER:~
+scp docker-compose.yml $USER@$MANAGER:~
+scp -r traefik.yml $USER@$MANAGER:~
 echo -e "${GREEN}[+]${RESET} Archivos copiados"
 
-server_ip=$(ssh root@$MANAGER "hostname -I | cut -d' ' -f1")
+server_ip=$(ssh $USER@$MANAGER "hostname -I | cut -d' ' -f1")
 
 echo -e "${YELLOW}[*]${RESET} Desplegando servicios"
-ssh root@$MANAGER "docker stack deploy -c ~/docker-compose.yml project"
+ssh $USER@$MANAGER "docker stack deploy -c ~/docker-compose.yml project"
 echo -e "${GREEN}[+]${RESET} Servicios desplegados. Accede al dashboard en http://$server_ip:8080"
 
 echo -e "${GREEN}[+]${RESET} Configuración finalizada"
